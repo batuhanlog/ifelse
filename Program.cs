18a168590948f4fb6dca4d019864e5ec2dff9c09
batuhanlog <mrkayahan66@gmail.com>
2022-12-27 00:20:25 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           int time = DateTime.Now.Hour;
+
+           if (time >=6 && time <11)
+            Console.Write("Hoş Geldin");
+           else if(time <= 18)
+           Console.WriteLine("İyi Günler!");
+           else
+           Console.WriteLine("İyi Geceler!");
+
+           String sonuc = time <=18 ? "İyi günler" : "İyi Geceler!";
+           
+           sonuc=time>=6 && time <11 ? "Günaydın" : time<=18 ? "İyi Günler!" : "iyi Geceler";
+           Console.WriteLine(sonuc);
+
+           
+        }
+    }
+}
